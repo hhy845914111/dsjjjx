197bd9075e1ac0b0aecc87f90b42ef1c49d34d9e
hhy845914111 <hoorex@outlook.com>
2016-11-02 18:36:23 +0800

    Update home_work15.py

--- home_work15.py
+++ home_work15.py
@@ -1,7 +1,13 @@
 from scipy import stats
 from douban1 import douban
+from math import sqrt
 
 rank_lst = [float(i[3]) for i in douban()]
-print stats.kstest(rank_lst, 'norm')
+
+mean = sum(rank_lst) / len(rank_lst)
+stdev = sqrt(sum([(i - mean)**2 for i in rank_lst]))
+stdrk = [((i - mean) / stdev) for i in rank_lst]
+             
+print stats.kstest(stdrk, 'norm')
 print "不服从正态分布"
 
